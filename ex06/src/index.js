d0fb8e0cf9a69c256d2a270fcf80cfbd6dc8dd77
Elvira Neziri <neziiri.elvira@gmail.com>
2021-03-31 01:05:24 +0200

    Added exercises from Day23

---
+++ ex06/src/index.js
@@ -0,0 +1,13 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+// remove code below this line
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
